2d2b45ed2a6808e5669f6b101212ad9164615b41
Ludovico O. Russo <ludus.russo@gmail.com>
2018-03-16 12:25:44 +0100

    up makefile and blink.c

--- blink.c
+++ blink.c
@@ -1,11 +1,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
 int main(void) {
-   DDRA = 0B10000000;
+   DDRA |= 0B10000000;
    while (1) {
        PORTA ^= 0B10000000;
-        _delay_ms(50);
+        _delay_ms(50); // 10Hz
       }
    }
